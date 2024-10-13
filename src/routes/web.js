f7d589164b4f3a8dfe8ce674cbd5992120d87cd1
vixuancu <vixuancu2004@gmail.com>
2024-10-13 23:13:08 +0700

    #3. MVC Design Pattern - Áp Dụng Mô Hình MVC

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,13 @@
 import express from "express";
-
+import homeController from "../controller/homeController";
 const router = express.Router();
+
 // express app
 const initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello world");
-  });
+  // path , handler
+  router.get("/", homeController.handleHelloWorld);
+  router.get("/user", homeController.handleUserPage);
+
   return app.use("/", router);
 };
 
