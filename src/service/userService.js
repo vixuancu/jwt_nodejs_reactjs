e3269be4f1ed00bcc78c06aad14d5a6789eb9844
vixuancu <vixuancu2004@gmail.com>
2024-10-19 22:18:29 +0700

    #8.4 Relationships - Tạo Mối Quan Hệ Giữa Các Models

--- src/service/userService.js
+++ src/service/userService.js
@@ -26,6 +26,23 @@ const createNewUser = async (email, password, username) => {
   }
 };
 const getUserList = async () => {
+  // test relationship
+  let newUser = await db.User.findOne({
+    where: { id: 1 },
+    attributes: ["id", "username", "email"],
+    include: { model: db.Group, attributes: ["name", "description"] },
+    raw: true,
+    nest: true,
+  });
+  let roles = await db.Role.findAll({
+    include: { model: db.Group, where: { id: 1 } },
+
+    raw: true,
+    nest: true,
+  });
+  console.log("check new user", newUser);
+  console.log("check new Roles", roles);
+
   let users = [];
 
   users = await db.User.findAll();
