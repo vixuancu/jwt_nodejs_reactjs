1c28f86824f641f5e2686db7b4d8ad87a2ebdd03
vixuancu <vixuancu2004@gmail.com>
2024-11-10 20:09:13 +0700

    #10.1.3 Axios Post Data - Test APIs với React

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -4,6 +4,10 @@ const testApi = (req, res) => {
     data: "test api",
   });
 };
+const handleRegister = (req, res) => {
+  console.log("call me", req.body);
+};
 module.exports = {
   testApi,
+  handleRegister,
 };
--- src/server.js
+++ src/server.js
@@ -1,36 +1,16 @@
 import express from "express";
 import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
+import initApiRoutes from "./routes/api";
+import configCors from "./config/cors";
 require("dotenv").config();
 import bodyParser from "body-parser";
 // import connection from "./config/connectDB";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", process.env.REACT_URL);
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
+// config Cors
+configCors(app);
 // config view engine
 configViewEngine(app);
 
@@ -40,8 +20,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // test connection db
 // connection();
 // init webRoutes
-
 initWebRoutes(app);
+initApiRoutes(app);
 //
 
 app.listen(PORT, () => {
