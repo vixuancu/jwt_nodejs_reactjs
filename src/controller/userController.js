bdf04274c1b964dc9d7b3dfdd076aaa6bb56b57c
vixuancu <vixuancu2004@gmail.com>
2024-11-14 23:48:20 +0700

    #10.6.5 Create New User - Tạo Người Dùng

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -31,7 +31,13 @@ const readFunc = async (req, res) => {
 };
 const createFunc = async (req, res) => {
   try {
-    let users = await userApiService.createNewUser();
+    // validate backend chưa làm
+    let data = await userApiService.createNewUser(req.body);
+    return res.status(200).json({
+      EM: data.EM, // error message
+      EC: data.EC, //error code
+      DT: data.DT, // data
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
--- src/service/userApiService.js
+++ src/service/userApiService.js
@@ -55,7 +55,12 @@ const getUserWithPagination = async (page, limit) => {
 };
 const createNewUser = async (data) => {
   try {
-    await db.User.create({});
+    await db.User.create(data);
+    return {
+      EM: "created success",
+      EC: 0,
+      DT: [],
+    };
   } catch (error) {
     console.log(error);
     return {
