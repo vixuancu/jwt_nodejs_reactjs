5e9006282c89d101b792b283a26649c0d4fb2fee
vixuancu <vixuancu2004@gmail.com>
2024-12-10 11:45:53 +0700

    #12.7 Assign Roles to Group - Gán Quyền Cho Group

--- src/controller/roleController.js
+++ src/controller/roleController.js
@@ -90,11 +90,28 @@ const deleteFunc = async (req, res) => {
     });
   }
 };
-
+const assignRoleToGroup = async (req, res) => {
+  try {
+    let data = await roleApiService.assignRoleToGroup(req.body.data);
+    return res.status(200).json({
+      EM: data.EM, // error message
+      EC: data.EC, //error code
+      DT: data.DT, // data
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      EM: "error from sever", // error message
+      EC: -1, //error code
+      DT: "", // data
+    });
+  }
+};
 module.exports = {
   readFunc,
   createFunc,
   updateFunc,
   deleteFunc,
   getRoleByGroup,
+  assignRoleToGroup,
 };
--- src/service/roleApiService.js
+++ src/service/roleApiService.js
@@ -109,9 +109,30 @@ const getRoleByGroup = async (id) => {
     };
   }
 };
+const assignRoleToGroup = async (data) => {
+  try {
+    await db.Group_Role.destroy({
+      where: { groupId: +data.groupId },
+    });
+    await db.Group_Role.bulkCreate(data.groupRoles);
+    return {
+      EM: "Assign Role To Group success  hooh...",
+      EC: 0,
+      DT: [],
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      EM: "something wrong with service",
+      EC: 1,
+      DT: [],
+    };
+  }
+};
 module.exports = {
   createNewRoles,
   getAllRoles,
   deleteRole,
   getRoleByGroup,
+  assignRoleToGroup,
 };
